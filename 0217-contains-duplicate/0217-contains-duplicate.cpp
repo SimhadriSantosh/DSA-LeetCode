b5cb5c677ecf74d6ac7f535e7d7f800c5701032b
Simhadri Santosh <santoshsimhadri2211@gmail.com>
2023-09-09 22:31:48 +0530

    Time: 139 ms (31.92%), Space: 70.8 MB (29.54%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>s;
+        for(auto i:nums){
+            if(s.find(i)!=s.end())return 1;
+            s.insert(i);
+        }
+        return 0;
+    }
+};
